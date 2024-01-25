9d8f6a1ddd316e28572460e334fcb51842055e9e
Gowtham A V <129061199+av-gowtham@users.noreply.github.com>
2024-01-25 11:26:01 +0530

    Update stop-watch.js

--- stop-watch.js
+++ stop-watch.js
@@ -79,7 +79,7 @@ resetElement.addEventListener('click', () => {
 function reset(){
   clearInterval(intervalID);
   startElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-timer"><line x1="10" x2="14" y1="2" y2="2"/><line x1="12" x2="15" y1="14" y2="11"/><circle cx="12" cy="14" r="8"/></svg><p >Start</p>';
-  startElement.classList.remove('stop');
+  startElement.classList.remove('pause');
   resetElement.classList.remove('reset');
   lapDivElement.innerHTML = '<div class="list"></div>';
   milliseconds = 0;
